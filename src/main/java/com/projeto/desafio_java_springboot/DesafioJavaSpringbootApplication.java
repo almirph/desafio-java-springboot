be5918cd3fbf9629cb3f74519612a129076f4698
almir.arruda <almir.arruda@ntendencia.com.br>
2021-03-21 19:24:51 -0300

    commit inicial

---
+++ src/main/java/com/projeto/desafio_java_springboot/DesafioJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.desafio_java_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioJavaSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projeto/desafio_java_springboot/DesafioJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.desafio_java_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
